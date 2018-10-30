b3b50c550321a00f63cd4102358be5c4ab5ecc5e
Ian Gordon <gordonianjames@icloud.com>
2018-10-30 02:45:41 -0700

    Add some style via reactstrap components

---
+++ newClient/src/App.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import './App.css'
+
+import { Col, Container, Jumbotron, Row } from 'reactstrap'
+import { ChartReview } from './ChartReview'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <Jumbotron> 
+        <Container>
+          <Row>
+            <Col>
+              <ChartReview />
+            </Col>
+        </Row>
+        </Container> 
+      </Jumbotron>
+    )
+  }
+}
+
+export default App
