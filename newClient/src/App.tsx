569b91701d25878b6dfcf0e29d4309d9bf96d515
Ian Gordon <gordonianjames@icloud.com>
2018-11-03 11:38:51 -0700

    The Hospital field is always present on the chart review form for Piedmont (https://trello.com/c/DUuwkfmv)

--- newClient/src/App.tsx
+++ newClient/src/App.tsx
@@ -3,15 +3,19 @@ import './App.css'
 
 import { Col, Container, Jumbotron, Row } from 'reactstrap'
 import { ChartReview } from './ChartReview'
+import { config } from './config.js'
 
 class App extends React.Component {
+  public state = {
+    customer: config.customer
+  } 
   public render() {
     return (
       <Jumbotron> 
         <Container>
           <Row>
             <Col>
-              <ChartReview />
+              <ChartReview customer={this.state.customer} />
             </Col>
         </Row>
         </Container> 
--- newClient/src/ChartReview.tsx
+++ newClient/src/ChartReview.tsx
@@ -3,18 +3,14 @@ import { ChartReviewForm } from './ChartReviewForm';
 
 // https://github.com/piotrwitek/react-redux-typescript-guide#stateful-components---class
 
-interface IChartReviewState {
-  readonly count: number;
+interface IChartReviewProps {
+  customer: string
 }
 
-export class ChartReview extends React.Component<any, IChartReviewState> { 
-  public readonly state: IChartReviewState = {
-    count: 0,
-  };
-
+export class ChartReview extends React.Component<IChartReviewProps, {}> { 
   public render() {
     return (
-      <ChartReviewForm />
+      <ChartReviewForm customer={this.props.customer} />
     );
   }
 }
