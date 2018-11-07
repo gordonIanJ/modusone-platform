d73f4152a28051aac726b7be5cbe4273c1285fb3
Ian Gordon <gordonianjames@icloud.com>
2018-11-07 14:27:51 -0800

    Navigation is improved

--- newClient/src/App.tsx
+++ newClient/src/App.tsx
@@ -1,19 +1,86 @@
 import * as React from 'react'
 import './App.css'
 
-// import { Col, Collapse, Container, Jumbotron, Nav, Navbar, NavItem, NavLink, Row } from 'reactstrap'
-import { Col, Row } from 'reactstrap'
-import { ChartReview } from './ChartReview/ChartReview'
+import { CHI } from 'src/ChartReview/CHI'
 
-class App extends React.Component {
+import { 
+  Collapse, 
+  Container,
+  /*DropdownItem,
+  DropdownMenu,
+  DropdownToggle,*/ 
+  Jumbotron, 
+  Nav, 
+  Navbar,
+  NavbarBrand, 
+  NavItem, 
+  NavLink,
+  // UncontrolledDropdown 
+} from 'reactstrap'
+
+import { BrowserRouter as 
+  Router, 
+  Route } from 'react-router-dom'
+import './index.css'
+
+import { Home } from './ChartReview/Home'
+
+import NavbarToggler from 'reactstrap/lib/NavbarToggler';
+import { Piedmont } from 'src/ChartReview/Piedmont'
+
+interface IAppProps {
+  knockKnock: string
+}
+
+interface IAppState {
+  isOpen: boolean
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
   public render() {
     return (
-      <Row>
-        <Col>
-          <ChartReview customer='CHI' />
-        </Col>
-      </Row>
+  <Router>
+    <div>
+    <Navbar color="light" light={true} expand="md">
+          <NavbarBrand href="/">ModusOne Health</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar={true}>
+            <Nav className="ml-auto" navbar={true}>
+              <NavItem>
+                <NavLink href="/Piedmont/">Piedmont</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/CHI/">CHI</NavLink>
+              </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+      <Jumbotron> 
+        <Container>
+          <div>
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/chi" component={CHI} />
+            <Route path="/piedmont" component={Piedmont} />
+          </div>
+        </Container> 
+      </Jumbotron>
+      </div>
+  </Router>
     )
+  }
+  
+  private toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 }
 
---
+++ newClient/src/ChartReview/Home.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { 
+  ButtonDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  NavLink
+} from 'reactstrap'
+
+
+
+interface IHomeState {
+  dropdownOpen: boolean
+}
+
+// export const Home: React.SFC = (props) => {
+export class Home extends React.Component<any, IHomeState> {
+  constructor(props: any) {
+    super(props);
+   
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+  
+  public render() {
+    return(
+      <div> 
+      <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret={true}>
+          Select a customer 
+        </DropdownToggle>
+        <DropdownMenu>
+        <DropdownItem>
+          <NavLink href="/Piedmont/">Piedmont</NavLink>
+        </DropdownItem>
+        <DropdownItem>
+          <NavLink href="/CHI/">CHI</NavLink>
+        </DropdownItem>
+        </DropdownMenu>
+      </ButtonDropdown>
+      </div>
+    )
+  }
+    
+  private toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+}
